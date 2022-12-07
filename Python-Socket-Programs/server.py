ee148e4e4ef3bb203eadc06545a61a8529263501
Emmy Tran <100542601+emmytran@users.noreply.github.com>
2022-12-06 22:22:25 -0800

    Update server.py

--- Python-Socket-Programs/server.py
+++ Python-Socket-Programs/server.py
@@ -1,57 +1,68 @@
-import socket
 import threading
+import signal
+import socket
+import sys
 
+server = socket.gethostbyname(socket.gethostname())
+port = 5050
+address = (server, port)
+clients = []
+ONLINE = threading.Event()
 
-PORT = 2412
-SERVER = socket.gethostbyname(socket.gethostname())
-ADDR = (SERVER,PORT)
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
+def main():
+    global ONLINE
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        server.bind(address)
+        server.listen()
+        ONLINE.set()
 
-server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server.bind(ADDR)
-OTHERS = []
+        print(f'\nServer Connected!\nPORT NUMBER: {port}\n')
+        print(f"SERVER INFO: {server}")
+    except:
+        return print(f'\nFAILED TO START ON PORT NUMBER: {port}\n\n')
 
-def SENDMSG(txt,new):
-    for PERSON in OTHERS:
-        if PERSON == new:
-            pass
-        else:
-            PERSON.send(txt.encode(FORMAT))
-
-
-def client_handle(conn, addr):
-    print(f'[NEW CONNECTION] {addr} conneted')
-    connected = True
-    while connected:
-        msg_length = conn.recv(2048).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            if msg == DISCONNECT_MESSAGE:
-                connected = False
-                OTHERS.remove(conn)
-
-            print(f'[{addr}] {msg}')
-            SENDMSG(
-                f'___________________________________________\n'
-                f'[NEW MESSAGE]New message from {addr[0]} :\n{msg}\n'
-                f'___________________________________________\n',conn)
-
-    conn.close()
-
-def start():
-    server.listen()
-    print(f'[LISTENING] Sever is listening on {SERVER}')
     while True:
-        conn, addr = server.accept()
-        thread = threading.Thread(target=client_handle, args=(conn, addr))
-        thread.start()
-        OTHERS.append(conn)
-        SENDMSG(f'___________________________________________\n'
-                f'[NEW CONNECTION]{addr[0]} connected'
-                f'\n___________________________________________\n', conn)
-        print(f'\n[ACTIVE CONNECTIONS] {threading.activeCount() - 1}')
+        client, addr = server.accept()
+        print(f'Entered chat: {addr}')
+        clients.append(client)
 
-print('[STARTING] server is starting....')
-start()
+        thr = threading.Thread(target=recv_msg, args=[client])
+        thr.daemon = True
+        thr.start()
+
+    while ONLINE.is_set():
+        pass
+
+def recv_msg(client):
+    while True:
+        try:
+            msg = client.recv(2048)
+            broadcast(msg, client)
+        except:
+            client.send('!BYE'.encode('utf-8'))
+            remove_client(client)
+            break
+
+def broadcast(msg, client):
+    for user in clients:
+        if not user == client:
+            try:
+                user.send(msg)
+            except:
+                remove_client(user)
+
+def remove_client(client):
+    if client in clients:
+        clients.remove(client)
+
+def close_server(signum, frame):
+    global ONLINE
+
+    ONLINE.clear()
+    print('\rCLOSING SERVER ...')
+    exit(0)
+
+signal.signal(signal.SIGINT, close_server)
+
+main()
