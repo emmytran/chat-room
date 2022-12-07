dc3c5ab1073f9bf4ce445ee4f4fd4534860e1f3a
Emmy Tran <100542601+emmytran@users.noreply.github.com>
2022-12-06 22:22:11 -0800

    Update client.py

--- Python-Socket-Programs/client.py
+++ Python-Socket-Programs/client.py
@@ -1,42 +1,80 @@
-import socket
-import os
 import threading
+import socket
+import signal
+from time import sleep
+
+server = input('What is the server IP:')
+port = int(input('What is the server PORT:'))
+header = 64
+address = (server, port)
+CONNECT_SIGNAL = threading.Event()
+
+def main():
+    global CONNECT_SIGNAL
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        client.connect(address)
+        CONNECT_SIGNAL.set()
+    except:
+        return print('\nUnable to connect to the server')
+
+    print(f'\n---------WELCOME TO INIFINITE CHAT--------\n')
+    username = input('Enter your name: ')
+    print('\n')
+
+    client.send(f'[WELCOME] {username} has entered the chatroom'.encode('utf-8'))
+
+    thread1 = threading.Thread(target=recv_msg, args=[client, username])
+    thread1.daemon = True
+#    thread1.setDaemon(True)
+    thread1.start()
+
+    thread2 = threading.Thread(target=send_msg,args=[client, username])
+    thread2.daemon = True
+    thread2.start()
+
+    while CONNECT_SIGNAL.is_set():
+        pass
+
+    send_msg_client_exit(client, username)
+    client.close()
+
+def recv_msg(client, username):
+    global CONNECT_SIGNAL
+
+    while CONNECT_SIGNAL.is_set():
+        try:
+            msg = client.recv(2048).decode('utf-8')
+            
+            if msg != '!BYE':
+                print(f'\r{msg}\nYou: ', end='')
+
+            else:
+                CONNECT_SIGNAL.clear()
+        except:
+            print('\nDISCONNECTED')
+            CONNECT_SIGNAL.clear()
+
+def send_msg(client, username):
+    global CONNECT_SIGNAL
+
+    while CONNECT_SIGNAL.is_set():
+        try:
+            msg = input('You: ')
+            client.send(f'{username}: {msg}'.encode('utf-8'))
+        except:
+            CONNECT_SIGNAL.clear()
+
+def send_msg_client_exit(client, username):
+    client.send(f'\n[GOODBYE] {username} has left the chat\n'.encode('utf-8'))
+
+def close_connect(sigmun, frame):
+    global CONNECT_SIGNAL
+    CONNECT_SIGNAL.clear()
 
 
-WORKING = True
-SERVER = input('What is the server IP?')
-PORT = int(input('What is the server PORT?'))
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
-ADDR = (SERVER,PORT)
+signal.signal(signal.SIGINT, close_connect)
 
-client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-client.connect(ADDR)
-print('[CONNECTION] connected successfully')
-
-def send(msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' '*(2048 - len(send_length))
-    client.send(send_length)
-    client.send(message)
-
-
-
-
-
-def RECIVER():
-    while WORKING:
-        DATA = client.recv(2048).decode(FORMAT)
-        print(DATA)
-
-print(f"if you want to exit type : {DISCONNECT_MESSAGE}")
-THREAD = threading.Thread(target=RECIVER)
-THREAD.start()
-while WORKING:
-    MSG = input()
-    send(MSG)
-    if MSG == DISCONNECT_MESSAGE:
-        WORKING = False
-        exit()
+main()
